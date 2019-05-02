01635b1ba0529b127baabe944deb53b75daf0a28
MrAnderson91 <klodale9491@gmail.com>
2019-05-02 22:45:28 +0200

    aggiunta di modello convolutivo con leaky_relu e batch optimization

--- deep_models.py
+++ deep_models.py
@@ -38,7 +38,6 @@ def create_deep_model_2(n_cols, layers=5, neurons=100):
 
 # creazione modello 3 : ffnn con fully connected + leaky relu + batch optimization
 def create_deep_model_3(n_cols, classes, layers=5, neurons=100):
-    hidden_layers = layers
     num_neurons = neurons
     model = Sequential()
     # layer fully connected verso l'input
@@ -46,7 +45,7 @@ def create_deep_model_3(n_cols, classes, layers=5, neurons=100):
     model.add(LeakyReLU())
     model.add(BatchNormalization())
     # inserimento degli strati nascosti
-    for i in range(0, hidden_layers - 1):
+    for i in range(0, layers - 1):
         model.add(Dense(num_neurons, activation='linear'))
         model.add(LeakyReLU())
         model.add(BatchNormalization())
@@ -57,7 +56,7 @@ def create_deep_model_3(n_cols, classes, layers=5, neurons=100):
 
 
 # creazione del modello 4 : rete convolutiva 1D
-def create_deep_model_4(n_cols, n_rows, classes=16):
+def create_deep_model_4(n_cols, classes):
     model = Sequential()
     model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(n_cols, 1)))
     model.add(MaxPooling1D(pool_size=3, padding='same', strides=2))
@@ -66,6 +65,26 @@ def create_deep_model_4(n_cols, n_rows, classes=16):
     model.add(Dense(64, activation='linear'))
     model.add(Flatten())  #linearizza lo strato di uscita
     model.add(Dense(625, activation='linear'))
+    model.add(Dense(classes, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# creazione del modello 5 : rete convolutiva 1D + Leaky Relu + Batch Optimization
+def create_deep_model_5(n_cols, classes, layers):
+    model = Sequential()
+    # estrazione delle features tramite layers convolutivi
+    model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(n_cols, 1)))
+    model.add(MaxPooling1D(pool_size=3, padding='same', strides=2))
+    model.add(Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=3, padding='same', strides=2))
+    model.add(Dense(64, activation='linear'))
+    model.add(Flatten())  # linearizza lo strato di uscita
+    # classificazione mediante leaky_relu + batch optimization
+    for i in range(0, layers - 1):
+        model.add(Dense(125, activation='linear'))
+        model.add(LeakyReLU())
+        model.add(BatchNormalization())
     model.add(Dense(classes, activation='softmax'))
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
